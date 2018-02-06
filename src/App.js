5e8168483d5cf50e32acf7cda845279b18029738
Julien <julien.simonet@missnumerique.com>
2018-02-06 16:38:38 +0100

    design app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ export default class App extends Component {
     render() {
         return (
             <div className="board">
-                <Card symbol="heart" number="10" />
-                <Card symbol="diamond" number="13" />
+                <div className="board-middle">
+                    <div className="board-inner">
+                        <Card symbol="heart" number="10" />
+                        <Card symbol="diamond" number="13" />
+                    </div>
+                </div>
             </div>
         );
     }
